7123e859d8d585566bc94c6ced8a923a7fe89e1e
Ruben Barahona <102640680+CaddoMohammed@users.noreply.github.com>
2024-12-16 04:59:41 -0600

    Nueva estructura.

--- datos/ubicaciones.js
+++ datos/ubicaciones.js
@@ -1,36 +1,367 @@
-var ubicaciones = [
-    {a:[14.08501, -87.16222],b:"Edificio A1"},
-    {a:[14.08556, -87.16223],b:"Edificio A2"},
-    {a:[14.0861, -87.16178],b:"Edificio B1"},
-    {a:[14.08667, -87.16204],b:"Edificio B2"},
-    {a:[14.08675, -87.16347],b:"Edificio C1"},
-    {a:[14.08704, -87.16406],b:"Edificio C1 Anexo"},
-    {a:[14.08679, -87.16503],b:"Edificio C2"},
-    {a:[14.08647, -87.16543],b:"Edificio C3"},
-    {a:[14.08549, -87.1641],b:"Edificio D1"},
-    {a:[14.08385, -87.16221 ],b:"Edificio E1"},
-    {a:[14.08499, -87.16421],b:"Edificio F1"},
-    {a:[14.08364, -87.16594],b:"Edificio G1"},
-    {a:[14.08383, -87.16675],b:"Edificio H1"},
-    {a:[14.08409, -87.16747],b:"Edificio I1"},
-    {a:[14.08611, -87.16665],b:"Edificio J1"},
-    {a:[14.0877, -87.15948],b:"Edificio K1"},
-    {a:[14.08782, -87.15993],b:"Edificio K2"},
-    {a:[14.08509, -87.16495],b:"Edificio 1847"},
-    {a:[14.08388, -87.16157],b:"Alma Mater"},
-    {a:[14.08491, -87.16325],b:"Padre Trino"},
-    {a:[14.0841667,-87.165],b:"CISE"},
-    {a:[14.08469, -87.16877],b:"Complejo Deportivo Universitario"},
-    {a:[14.08376, -87.16273],b:"VOAE"},
-    {a:[14.0836111,-87.1625],b:"Oficinas VOAE"},
-
-
-/*
-    {a:[14.0853, -87.16319],b:"Biblioteca"},
-    {a:[14.08598, -87.16689],b:"Museo"},
-    {a:[14.08489, -87.17012],b:"Palacio de los Deportes"},
-    {a:[14.08376, -87.16273],b:"VOAE"},
-    {a:[14.0836111,-87.1625],b:"Oficinas VOAE"},
-    {a:[14.08469, -87.16877],b:"Complejo Deportivo Universitario"},
-    */
+var centros = [
+	{a:"Ciudad universitaria",b:[14.084657,-87.165792]},
+];
+var ubicaciones = [[
+		{
+			a:[14.08501, -87.16222],
+			b:"Edificio A1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[50,0,0]
+					},
+					{
+						texto:"Mensaje 2",
+						tamaño:6,
+						posicion:[6,1.25,-5],
+						rotacion:[0,20,0]
+					},
+					{
+						texto:"Mensaje 3",
+						tamaño:6,
+						posicion:[1,1.25,-7],
+						rotacion:[0,0,50]
+					},
+					{
+						texto:"Mensaje 4",
+						tamaño:6,
+						posicion:[3,1.25,-5],
+						rotacion:[0,0,0]
+					},
+					{
+						texto:"Mensaje 5",
+						tamaño:6,
+						posicion:[6,1.25,-4],
+						rotacion:[0,0,0]
+					}
+				]
+			}
+		},
+		{
+			a:[14.08556, -87.16223],
+			b:"Edificio A2",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.0861, -87.16178],
+			b:"Edificio B1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08667, -87.16204],
+			b:"Edificio B2",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08675, -87.16347],
+			b:"Edificio C1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08704,-87.16406],
+			b:"Edificio C1 Anexo",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08679,-87.16503],
+			b:"Edificio C2",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08647,-87.16543],
+			b:"Edificio C3",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08549,-87.1641],
+			b:"Edificio D1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08385,-87.16221 ],
+			b:"Edificio E1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08499,-87.16421],
+			b:"Edificio F1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08364,-87.16594],
+			b:"Edificio G1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08383,-87.16675],
+			b:"Edificio H1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08409,-87.16747],
+			b:"Edificio I1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08611,-87.16665],
+			b:"Edificio J1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.0877,-87.15948],
+			b:"Edificio K1",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08782,-87.15993],
+			b:"Edificio K2",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08509,-87.16495],
+			b:"Edificio 1847",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08388,-87.16157],
+			b:"Alma Mater",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08491,-87.16325],
+			b:"Padre Trino",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.0841667,-87.165],
+			b:"CISE",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08469,-87.16877],
+			b:"Complejo Deportivo Universitario",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.08376, -87.16273],
+			b:"VOAE",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+		{
+			a:[14.0836111,-87.1625],
+			b:"Oficinas VOAE",
+			c:{
+				mostrarMensaje:true,
+				mensajes:[
+					{
+						texto:"Mensaje 1",
+						tamaño:6,
+						posicion:[-1.50,1.25,-5],
+						rotacion:[0,0,0]
+				}]
+			}
+		},
+	],
 ];
