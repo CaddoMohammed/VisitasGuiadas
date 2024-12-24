a5e9099542d5ca0544b5aafcd733acf09d7a6a1e
Ruben Barahona <102640680+CaddoMohammed@users.noreply.github.com>
2024-12-23 19:17:54 -0600

    Update ubicaciones.js

--- datos/ubicaciones.js
+++ datos/ubicaciones.js
@@ -1,23 +1,33 @@
 var centros = [
-	{a:"Ciudad universitaria",b:[14.084657,-87.165792]},
+	{a:"Ciudad universitaria",b:[14.084657,-87.165792],c:17,d:5,e:18,f:false,g:false},
 ];
 var ubicaciones = [[
 		{
 			a:[14.08501, -87.16222],
 			b:"Edificio A1 - Facultad de Ciencias Sociales",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Edificio A1: Facultad de Ciencias Sociales",
+						texto:"Edificio A1 - pertenece a la Facultad de Ciencias Sociales. Encuentras carreras como Trabajo Social, Sociología, Antrología, Historia, entre otras.",
 						tamaño:15,
 						posicion:"6 1.25 -10",
 						rotacion:"0 10 0",
 						fuente:"kelsonsans",
-						color:"#47DFFF"
+						color:"#eb4034"
 					},
-					{	
+					{
+						tipo:1,
+						texto:"Edificio A2 - pertenece a la Facultad de Ciencias Jurídicas. Es hogar de la carrera más antigua de la UNAH - Derecho​.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+					/*{	
 						tipo:2,
 						texto:"Mensaje 2",
 						tamaño:6,
@@ -43,7 +53,7 @@ var ubicaciones = [[
 						alto:7,
 						posicion:"35 1.25 -3",
 						rotacion:"0 -30 0",
-					}
+					}*/
 
 				]
 			}
@@ -51,348 +61,508 @@ var ubicaciones = [[
 		{
 			a:[14.08556, -87.16223],
 			b:"Edificio A2 - Facultad de Ciencias Jurídicas",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio A1 - pertenece a la Facultad de Ciencias Sociales. Encuentras carreras como Trabajo Social, Sociología, Antrología, Historia, entre otras.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+					{
+						tipo:1,
+						texto:"Edificio A2 - pertenece a la Facultad de Ciencias Jurídicas. Es hogar de la carrera más antigua de la UNAH - Derecho​.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.0861, -87.16178],
-			b:"Edificio B1 - Facultad de Ingeniería",
-			c:{
+			b:"Edificio B1",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50,1.25,-5",
-						rotacion:"0,0,0"
-				}]
+						texto:"Edificio B1 - Encuentras los laboratorios de la Facultad de Ingeniería.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+					{
+						tipo:1,
+						texto:"Edificio B2 - Pertenece a la Facultad de Ingeniería. Encuentras 5 de las 6 ingenierías con que cuenta este campus​.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08667, -87.16204],
-			b:"Edificio B2 - Facultad de Ingeniería",
-			c:{
+			b:"Edificio B2",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Edificio A1: Facultad de Ciencias Sociales",
+						texto:"Edificio B1 - Encuentras los laboratorios de la Facultad de Ingeniería.",
 						tamaño:15,
-						posicion:"0 0 -0",
-						rotacion:"0 0 0",
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
 						fuente:"kelsonsans",
-						color:"#4287f5"
-				}]
+						color:"#eb4034"
+					},
+					{
+						tipo:1,
+						texto:"Edificio B2 - Pertenece a la Facultad de Ingeniería. Encuentras 5 de las 6 ingenierías con que cuenta este campus​.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08675, -87.16347],
-			b:"Edificio C1 - Facultad de Ciencias Económicas",
-			c:{
+			b:"Edificio C1",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio C1 - La Facultad de Ciencias Económicas, Administrativas y Contables es la más grande de la U. Cuenta con más de 10 carreras.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08704,-87.16406],
 			b:"Edificio C1 Anexo",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio Anexo - Inaugurado en el 2019, alberga los departamentos de Mercadotecnia, Economía y Contaduría Pública.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08679,-87.16503],
 			b:"Edificio C2",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio C2:Perteneciente a la Facultad de Ciencias Económicas Administrativas y Constables. Además de encontrar carreras como Comercio Internacional o Informática Administrativa, alberga el Instituto de Investigaciones Económicas y Sociales.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08647,-87.16543],
-			b:"Edificio C3 - Facultad de Humanidades y Artes",
-			c:{
+			b:"Edificio C3",
+			//c:"10 270 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Edificio C3
+
+						Fue re-inaugurado en 2018 como un edificio inteligente. Actualmente encontrarás las carreras de Musica y Arquitectura. Ambas pertenecientes a la Facultad de Humanidades y Artes.`,
+						tamaño:15,
+						posicion:"8 -15 -30",
+						rotacion:"-45 -35 -20",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08549,-87.1641],
 			b:"Edificio D1",
-			c:{
+			c:"10 270 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:[-1.50,1.25,-5],
-						rotacion:[0,0,0]
-				}]
+						texto:`Edificio D1
+						
+						El D1 y F1 son los edificios gemelos. En el D1 encontrarás carreras como Ingeniería Civil y Pedagogogía. Asimismo encuentas el Teatro Padre Trino.`,
+						tamaño:15,
+						posicion:"-10 -1 -20",
+						rotacion:"10 -15 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08385,-87.16221 ],
-			b:"Edificio E1 - Facultad de Ciencias",
-			c:{
+			b:"Edificio E1",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio E1 - El edificio E1 es la sede de la Facultad de Ciencias y encontrarás la carrera de Física. Es uno de los edificios más antiguos de CU construido a mediados del Siglo XX.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08499,-87.16421],
 			b:"Edificio F1",
-			c:{
+			c:"0 210 -5",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Edificio F1
+
+						En el F1 encuentras carreras como Psicologia, Matematicas, Lenguas Extranjeras y Filosofia. Asimismo, esta el Centro de Apoyo Psicologico (CAPS).`,
+						tamaño:15,
+						posicion:"-5.5 -2 -25",
+						rotacion:"4 -10 2",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+					{
+						tipo:1,
+						texto:`Edificio D1
+						
+						El D1 y F1 son los edificios gemelos. En el D1 encontrarás carreras como Ingeniería Civil y Pedagogogía. Asimismo encuentas el Teatro Padre Trino.`,
+						tamaño:15,
+						posicion:"25 2 -20",
+						rotacion:"25 -70 -10",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08364,-87.16594],
-			b:"Edificio G1 - Facultad de Odontología",
-			c:{
+			b:"Edificio G1",
+			c:"10 190 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Edificio G1
+
+						El G1 pertenece a la Facultad de Odontologia. Es un centro de vinculacion de la U con la sociedad donde puedes realizarte tratamientos dentales a bajo costo.`,
+						tamaño:15,
+						posicion:"-6 25 -20",
+						rotacion:"45 -3 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08383,-87.16675],
 			b:"Edificio H1",
-			c:{
+			c:"5 240 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Edificio H1
+
+						El H1 cuenta con carreras como Nutricion, Radiotecnologia, Terapia Funcional y Fonoaudiologia. Tambien encontraras el CDIBIR donde puedes realizarte examenes biomedicos.`,
+						tamaño:15,
+						posicion:"-8 -6 -14",
+						rotacion:"-60 24 -15",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08409,-87.16747],
-			b:"Edificio I1 - Facultad de Química y Farmacia",
-			c:{
+			b:"Edificio I1",
+			c:"10 260 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Edificio I1
+
+						Pertenece a la Facultad de Quimica y Farmacia. Tambien se encuentra el Centro de Informacion Toxicologico (CENTOX).​`,
+						tamaño:15,
+						posicion:"-10 12 -25",
+						rotacion:"-5 5 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08611,-87.16665],
 			b:"Edificio J1",
-			c:{
+			c:"0 220 -5",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Edificio J1
+
+						En el J1 encontraras las carreras de Biologia y Microbiologia. Cuenta con e1 Centro de Investigaciones Geneticas, dos museos y las clinicas de salud VOAE.`,
+						tamaño:15,
+						posicion:"-7 -2 -13",
+						rotacion:"-1 0 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.0877,-87.15948],
-			b:"Edificio K1 - Facultad de Ciencias Espaciales",
-			c:{
+			b:"Edificio K1",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio K1 - Pertenece a la Facultad de Ciencias Espaciales (FACES). Esta fue una de las Facultades más recientes en crearse. En este edificio encontrarás la Carrera de Astronomía y Astrofísica.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+					{
+						tipo:1,
+						texto:"Edificio K2 - El edificio K2 también pertenece a FACES. Carreras como Ciencia y Tecnología de la Información Geográfica, Operaciones Aeronáuticas entre otras las encuentras acá.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08782,-87.15993],
-			b:"Edificio K2 - Facultad de Ciencias Espaciales",
-			c:{
+			b:"Edificio K2",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:[-1.50,1.25,-5],
-						rotacion:[0,0,0]
-				}]
+						texto:"Edificio K1 - Pertenece a la Facultad de Ciencias Espaciales (FACES). Esta fue una de las Facultades más recientes en crearse. En este edificio encontrarás la Carrera de Astronomía y Astrofísica.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+					{
+						tipo:1,
+						texto:"Edificio K2 - El edificio K2 también pertenece a FACES. Carreras como Ciencia y Tecnología de la Información Geográfica, Operaciones Aeronáuticas entre otras las encuentras acá.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08509,-87.16495],
 			b:"Edificio 1847",
-			c:{
+			c:"0 220 -6",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Edificio 1847
+						
+						Su nombre se debe al año de Fundación de nuestra Universidad. El Edificio 1847 fue inaugurado en 2019, en ella se albergan diferentes postgrados así como el comedor universitario.`,
+						tamaño:15,
+						posicion:"-6 -2 -7",
+						rotacion:"-80 0 -1",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08388,-87.16157],
 			b:"Alma Mater",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio D1 - El D1 y F1 son los edificios gemelos. En el D1 encontrarás carreras como Ingeniería Civil y Pedagogogía. Asimismo encuentas el Teatro Padre Trino.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08491,-87.16325],
 			b:"Padre Trino",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio D1 - El D1 y F1 son los edificios gemelos. En el D1 encontrarás carreras como Ingeniería Civil y Pedagogogía. Asimismo encuentas el Teatro Padre Trino.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.0841667,-87.165],
 			b:"CISE",
-			c:{
+			c:"6 -90 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`CISE
+
+						Encontraras la Direccion de Admisiones y el Comisionado Universitario. Tambien cuenta con una cinemateca, un food court y bancos.`,
+						tamaño:15,
+						posicion:"-7 4 -20",
+						rotacion:"0 0 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08469,-87.16877],
 			b:"Complejo Deportivo Universitario",
-			c:{
+			c:"25 225 10",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:`Palacios de los deportes
+
+						Fue premiado a nivel mundial en el 2016 por Distincion por accesibilidad. Cuenta con mas de 30 mil metros^2 de construccion`,
+						tamaño:15,
+						posicion:"-7 -5 -25",
+						rotacion:"-13 -15 9",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.08376, -87.16273],
 			b:"Registro DIPP",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio D1 - El D1 y F1 son los edificios gemelos. En el D1 encontrarás carreras como Ingeniería Civil y Pedagogogía. Asimismo encuentas el Teatro Padre Trino.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 		{
 			a:[14.0836111,-87.1625],
 			b:"Oficinas VOAE",
-			c:{
+			//c:"0 -360 0",
+			d:{
 				mostrarMensaje:true,
 				mensajes:[
 					{
 						tipo:1,
-						texto:"Mensaje 1",
-						tamaño:6,
-						posicion:"-1.50 1.25 -5",
-						rotacion:"0 0 0"
-				}]
+						texto:"Edificio D1 - El D1 y F1 son los edificios gemelos. En el D1 encontrarás carreras como Ingeniería Civil y Pedagogogía. Asimismo encuentas el Teatro Padre Trino.",
+						tamaño:15,
+						posicion:"6 1.25 -10",
+						rotacion:"0 10 0",
+						fuente:"kelsonsans",
+						color:"#eb4034"
+					},
+				]
 			}
 		},
 	],
