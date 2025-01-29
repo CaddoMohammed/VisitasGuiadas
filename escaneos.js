9a4c6045566bd6e64224c00d582a01a880507d92
Ruben Barahona <102640680+CaddoMohammed@users.noreply.github.com>
2025-01-29 11:16:08 -0600

    Update escaneos.js

--- escaneos.js
+++ escaneos.js
@@ -1,5 +1,5 @@
 const Y = supabase.createClient("https://yrpewvflgexntamdzlia.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlycGV3dmZsZ2V4bnRhbWR6bGlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxMDUzOTIsImV4cCI6MjA1MjY4MTM5Mn0.VxNcklQ9I8_18NPl3EPo6rJWfGrmqfoU4rKT9BwWzSo");
-let I,J,X,Z;
+let I,J,X=0,Z;
 a1();
 if(navigator.geolocation){
 	navigator.geolocation.watchPosition(
@@ -15,41 +15,46 @@ if(navigator.geolocation){
 					let f = 2*Math.atan2(Math.sqrt(e),Math.sqrt(1-e));
 					let g = 6378*f;
                     if(g<x){
-                        console.log(x,i,j);
                         I = i;
                         J = j;
                         x = g;
                     }
                 }
-
             }
-            document.getElementById("escaneo").innerHTML = `
-                <a-scene mindar-image="imageTargetSrc:datos/${Z[I].Nombre}/${Z[I].Ubicaciones[J].Nombre}.mind" color-space="sRGB" renderer="colorManagement:true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false;">
-                    <a-assets>
-                    <a-asset-item id="bearModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/bear/scene.gltf"></a-asset-item>
-                    <a-asset-item id="raccoonModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/raccoon/scene.gltf"></a-asset-item>
-                    </a-assets>
-                
-                    <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-                
-                    <a-entity mindar-image-target="targetIndex: 0">
-                    <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#raccoonModel" animation-mixer>
-                    </a-entity>
-                    <a-entity mindar-image-target="targetIndex: 1">
-                    <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#bearModel" animation-mixer>
-                    </a-entity>
-                </a-scene>`;
+            if(X!==`${Z[I].Nombre}/${Z[I].Ubicaciones[J].Nombre}`){
+                    document.getElementById("escaneo").innerHTML = `
+                    <a-scene mindar-image="imageTargetSrc:datos/${Z[I].Nombre}/${Z[I].Ubicaciones[J].Nombre}.mind" color-space="sRGB" renderer="colorManagement:true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false;">
+                        <a-assets>
+                        <a-asset-item id="bearModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/bear/scene.gltf"></a-asset-item>
+                        <a-asset-item id="raccoonModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/raccoon/scene.gltf"></a-asset-item>
+                        </a-assets>
+                    
+                        <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+                    
+                        <a-entity mindar-image-target="targetIndex: 0">
+                        <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#raccoonModel" animation-mixer>
+                        </a-entity>
+                        <a-entity mindar-image-target="targetIndex: 1">
+                        <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#bearModel" animation-mixer>
+                        </a-entity>
+                    </a-scene>`;
+                let a = document.getElementsByClassName("inner");
+                a[0].classList.add("d-flex","align-items-center","justify-content-center");
+                if(!document.getElementById("imagen")){
+                    let b = document.createElement("img");
+                    b.id = "imagen";
+                    b.style.width = "95%";
+                    b.src = `img/referencias/${Z[I].Nombre}/${Z[I].Ubicaciones[J].Nombre}.webp`;
+                    a[0].appendChild(b);
+                }
+                X = `${Z[I].Nombre}/${Z[I].Ubicaciones[J].Nombre}`;
+            }
 		},
 		(x) => {
-			if(Z===null){
-                Z = Z.data;
-                localStorage.setItem("CaddoMohammed-VisitasGuiadasUNAH-CentroUniversitario",JSON.stringify(Z));
-
-			}
 		}, {
 			enableHighAccuracy:true,
 			maximumAge:0,
-			timeout:500
+			timeout:50
 		}
 	);
 }
