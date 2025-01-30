4bb9a7cd59bd6df3c18165422c9177abe60c998d
Ruben Barahona <102640680+CaddoMohammed@users.noreply.github.com>
2025-01-30 07:29:22 -0600

    Cambio de mindAR a ARjs.

--- escaneos.js
+++ escaneos.js
@@ -1,70 +1,24 @@
 const Y = supabase.createClient("https://yrpewvflgexntamdzlia.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlycGV3dmZsZ2V4bnRhbWR6bGlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxMDUzOTIsImV4cCI6MjA1MjY4MTM5Mn0.VxNcklQ9I8_18NPl3EPo6rJWfGrmqfoU4rKT9BwWzSo");
-let I,J,X=0,Z;
+let Z;
 a1();
-if(navigator.geolocation){
-	navigator.geolocation.watchPosition(
-		(position) => {
-            let x = 100;
-            for(let i=0;i<Z.length;i++){
-                for(let j=0;j<Z[i].Ubicaciones.length;j++){
-                    let a = Math.PI*position.coords.latitude/180;
-					let b = Math.PI*Z[i].Ubicaciones[j].Latitud/180;
-					let c = (a-b)/2;
-					let d = Math.PI*(position.coords.longitude-Z[i].Ubicaciones[j].Longitud)/360;
-					let e = Math.pow(Math.sin(c),2)+Math.cos(a)*Math.cos(b)*Math.pow(Math.sin(d),2)
-					let f = 2*Math.atan2(Math.sqrt(e),Math.sqrt(1-e));
-					let g = 6378*f;
-                    if(g<x){
-                        I = i;
-                        J = j;
-                        x = g;
-                    }
-                }
-            }
-            if(X!==`${Z[I].Nombre.replace(/\s+/g,"")}/${Z[I].Ubicaciones[J].Nombre.replace(/\s+/g,"")}`){
-                    document.getElementById("escaneo").innerHTML = `
-                    <a-scene mindar-image="imageTargetSrc:datos/${Z[I].Nombre.replace(/\s+/g,"")}/${Z[I].Ubicaciones[J].Nombre.replace(/\s+/g,"")}.mind" color-space="sRGB" renderer="colorManagement:true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false;">
-                        <a-assets>
-                        <a-asset-item id="bearModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/bear/scene.gltf"></a-asset-item>
-                        <a-asset-item id="raccoonModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/raccoon/scene.gltf"></a-asset-item>
-                        </a-assets>
-                    
-                        <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-                    
-                        <a-entity mindar-image-target="targetIndex: 0">
-                        <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#raccoonModel" animation-mixer>
-                        </a-entity>
-                        <a-entity mindar-image-target="targetIndex: 1">
-                        <a-gltf-model rotation="0 0 0 " position="0 -0.25 0" scale="0.05 0.05 0.05" src="#bearModel" animation-mixer>
-                        </a-entity>
-                    </a-scene>`;
-                    setTimeout(() => {
-                        let a = document.getElementsByClassName("inner");
-                        a[0].classList.add("d-flex","align-items-center","justify-content-center");
-                        if(!document.getElementById("imagen")){
-                            let b = document.createElement("img");
-                            b.id = "imagen";
-                            b.style.width = "95%";
-                            b.src = `img/referencias/${Z[I].Nombre.replace(/\s+/g,"")}/${Z[I].Ubicaciones[J].Nombre.replace(/\s+/g,"")}.webp`;
-                            a[0].appendChild(b);
-                        }
-                    },100);
-                X = `${Z[I].Nombre.replace(/\s+/g,"")}/${Z[I].Ubicaciones[J].Nombre.replace(/\s+/g,"")}`;
-            }
-		},
-		(x) => {
-		}, {
-			enableHighAccuracy:true,
-			maximumAge:0,
-			timeout:50
-		}
-	);
-}
 async function a1(){
 	try{
-		Z = await Y.rpc("DatosCentrosUniversitarios");
-        Z = Z.data;
-    }
+		Z = await Y.rpc("DatosCentrosUniversitariosPresencial");
+		Z = Z.data;
+		console.log(Z)
+		let a = document.querySelector("a-scene");
+		console.log(a)
+		for(let i=0;i<Z.length;i++){
+			for(let j=0;j<Z[i].Ubicaciones.length;j++){
+				let b = document.createElement("a-text");
+				b.setAttribute('gps-entity-place',`latitude:${Z[i].Ubicaciones[j].Latitud};longitude:${Z[i].Ubicaciones[j].Longitud};`);
+				b.setAttribute('look-at',`[gps-camera]`);
+				b.setAttribute('scale',`${Z[i].Ubicaciones[j].EscalaA} ${Z[i].Ubicaciones[j].EscalaB} ${Z[i].Ubicaciones[j].EscalaC}`);
+				b.setAttribute('value',`${Z[i].Ubicaciones[j].Texto}`);
+				a.appendChild(b);
+			}
+		}
+	}
 	catch(q){
 		console.error(q);
 	}
