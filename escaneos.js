28ba4f33e88007ec099b3130ae8d8bcaba89ee09
Ruben Barahona <102640680+CaddoMohammed@users.noreply.github.com>
2025-02-04 23:46:45 -0600

    Update escaneos.js

--- escaneos.js
+++ escaneos.js
@@ -8,14 +8,13 @@ async function a1(){
 		let a = document.querySelector("a-scene");
 		for(let i=0;i<Z.length;i++){
 			for(let j=0;j<Z[i].Ubicaciones.length;j++){
-				let b = document.createElement("a-text");
+				let b = document.createElement("a-entity");
+				b.setAttribute("geometry","primitive:plane; height:0; width:0");
+				b.setAttribute("material",`color:${Z[i].Ubicaciones[j].BackgroundColor}`);
 				b.setAttribute("look-at","[gps-camera]");
 				b.setAttribute("gps-entity-place",`latitude:${Z[i].Ubicaciones[j].Latitud};longitude:${Z[i].Ubicaciones[j].Longitud};`);
 				b.setAttribute("scale",`${Z[i].Ubicaciones[j].EscalaA} ${Z[i].Ubicaciones[j].EscalaB} ${Z[i].Ubicaciones[j].EscalaC}`);
-				b.setAttribute("value",`${Z[i].Ubicaciones[j].Nombre}\n\n${Z[i].Ubicaciones[j].Texto}`);
-				b.setAttribute("font","fonts/custom-msdf.json");
-				b.setAttribute("font-image","fonts/custom.png");
-				b.setAttribute("negate",false);
+				b.setAttribute("text",`value:${Z[i].Ubicaciones[j].Nombre}\n\n${Z[i].Ubicaciones[j].Texto}; color:${Z[i].Ubicaciones[j].TextColor}; font:chars/custom-msdf.json; font-image:chars/custom.png; negate:false`);
 				a.appendChild(b);
 			}
 		}
