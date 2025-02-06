fb8aadf5092619d5c32e866115eb192c481f587a
Ruben Barahona <102640680+CaddoMohammed@users.noreply.github.com>
2025-02-06 00:33:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 let V = 0; // mapa
 let W = localStorage.getItem("CaddoMohammed-VisitasGuiadasUNAH-CentroUniversitario"); // Centro seleccionado
-let X = new URLSearchParams(window.location.search); // Acceder a escaneo
+let X = null; // marcador de posicion en el mapa
 const Y = supabase.createClient("https://yrpewvflgexntamdzlia.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlycGV3dmZsZ2V4bnRhbWR6bGlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxMDUzOTIsImV4cCI6MjA1MjY4MTM5Mn0.VxNcklQ9I8_18NPl3EPo6rJWfGrmqfoU4rKT9BwWzSo");
 let Z;
 a1();
-let marker = null;
 if(navigator.geolocation){
 	navigator.geolocation.watchPosition(
 		(position) => {
-			if((marker!==null)&&(V!==0)){
-				V.removeLayer(marker);
+			if((X!==null)&&(V!==0)){
+				V.removeLayer(X);
 			}
 			if(W===null){
 				let x = false;
-				for(let i=0;i<Z.data.length;i++){
+				for(let i=0;i<Z.length;i++){
 					let a = Math.PI*position.coords.latitude/180;
-					let b = Math.PI*Z.data[i].Latitud/180;
+					let b = Math.PI*Z[i].Latitud/180;
 					let c = (a-b)/2;
-					let d = Math.PI*(position.coords.longitude-Z.data[i].Longitud)/360;
+					let d = Math.PI*(position.coords.longitude-Z[i].Longitud)/360;
 					let e = Math.pow(Math.sin(c),2)+Math.cos(a)*Math.cos(b)*Math.pow(Math.sin(d),2)
 					let f = 2*Math.atan2(Math.sqrt(e),Math.sqrt(1-e));
 					let g = 6378*f;
 					if(g<=15){
-						W = {A:Z.data[i]};
+						W = {A:Z[i]};
 						x = true;
 						break;
 					}
@@ -30,11 +29,11 @@ if(navigator.geolocation){
 				if(x===true){
 					b1(W.A);
 				} else {
-					b1(Z.data[0]);
+					b1(Z[0]);
 				}
 			}
 			if(V!==0){
-				marker = L.marker(
+				X = L.marker(
 					[position.coords.latitude,position.coords.longitude],{
 						icon: L.icon({
 							iconUrl:"img/bx-cross.svg",
@@ -47,31 +46,36 @@ if(navigator.geolocation){
 		},
 		(x) => {
 			if(W===null){
-				b1(Z.data[0]);
+				b1(Z[0]);
 			}
 		}, {
 			enableHighAccuracy:true,
 			maximumAge:0,
-			timeout:500
+			timeout:50
 		}
 	);
 }
 async function a1(){
 	try{
-		Z = await Y.rpc("DatosCentrosUniversitarios");
+		Z = await Y.rpc("DatosCentrosUniversitariosVirtual");
+		Z = Z.data;
 		document.getElementById("centros-universitarios").innerHTML = "";
-		for(let i=0;i<Z.data.length;i++){
+		for(let i=0;i<Z.length;i++){
 			let a = document.createElement("li");
 			a.className = "p-1";
 			document.getElementById("centros-universitarios").appendChild(a);
 			let b = document.createElement("a");
 			b.className = "dropdown-item p-1";
 			b.style.width = "width:max-content";
-			b.id = `centro${Z.data[i].Id}`;
-			b.innerHTML = Z.data[i].Nombre;
-			b.onclick = () => b1(Z.data[i]);
+			b.id = `centro${Z[i].Id}`;
+			b.innerHTML = Z[i].Nombre;
+			b.onclick = () => b1(Z[i]);
 			a.appendChild(b);
 		}
+		let a = document.createElement("li");
+		a.className = "p-1";
+		a.innerHTML = `<a class="dropdown-item p-1" style="width:max-content" href="escaneo.html">Exploracion</a>`
+		document.getElementById("centros-universitarios").appendChild(a);
 		if(W!==null){
 			W = JSON.parse(W);
 			b1(W.A);
@@ -84,17 +88,16 @@ async function a1(){
 function b1(x){
 	W = {A:x};
 	localStorage.setItem("CaddoMohammed-VisitasGuiadasUNAH-CentroUniversitario",JSON.stringify(W));
-	let a;
-	for(let i=0;i<Z.data.length;i++){
-		if(Z.data[i].Id!==x.Id){
-			document.getElementById(`centro${Z.data[i].Id}`).classList.remove("border-bottom");
-			document.getElementById(`centro${Z.data[i].Id}`).classList.remove("border-secondary");
-			document.getElementById(`centro${Z.data[i].Id}`).classList.remove("border-3");
+	for(let i=0;i<Z.length;i++){
+		if(Z[i].Id!==x.Id){
+			document.getElementById(`centro${Z[i].Id}`).classList.remove("border-bottom");
+			document.getElementById(`centro${Z[i].Id}`).classList.remove("border-secondary");
+			document.getElementById(`centro${Z[i].Id}`).classList.remove("border-2");
 		} else {
 			a = i;
-			document.getElementById(`centro${Z.data[i].Id}`).classList.add("border-bottom");
-			document.getElementById(`centro${Z.data[i].Id}`).classList.add("border-secondary");
-			document.getElementById(`centro${Z.data[i].Id}`).classList.add("border-2");
+			document.getElementById(`centro${Z[i].Id}`).classList.add("border-bottom");
+			document.getElementById(`centro${Z[i].Id}`).classList.add("border-secondary");
+			document.getElementById(`centro${Z[i].Id}`).classList.add("border-2");
 		}
 	}
 	document.getElementById("mapa").innerHTML = "";
@@ -117,6 +120,17 @@ function b1(x){
 		b.on("click",() => c1(x.Id,x.Ubicaciones[i].Id));
 	}
 }
-function c1(x,y){
-	console.log(x,y)
+async function c1(x,y){
+	try{
+		let a = await Y.storage.from("ubicaciones").getPublicUrl(`c${x}_u${y}.webp`);
+		document.getElementById("exploracion").click();
+		document.getElementById("foto360").innerHTML = `
+		<a-scene embedded>
+			<a-sky src="${a}"></a-sky>
+		</a-scene>`;
+
+	}
+	catch(q){
+		console.error(q);
+	}
 }
